65ce118a48339760d15b59108c1e2793253549d3
Eyrim <marfxedbot@gmail.com>
2023-09-06 12:33:21 +0100

    Completed exercise 2.1

---
+++ Calculator/src/Exercise2.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Exercise2 {
+    public static void exercise2() {
+        int[] nums = getRandomNums(2);
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        for (int i : nums) {
+            System.out.println(i);
+        }
+
+        System.out.println("Enter an operation to perform (+, *, -): ");
+        input = scanner.next();
+
+        System.out.println(calculate(input, nums));
+    }
+
+    private static int[] getRandomNums(int quantity) {
+        Random rand = new Random();
+
+        return rand.ints(quantity).toArray();
+    }
+
+    private static Integer calculate(String operation, int[] nums) {
+        switch (operation) {
+            case "+":
+                return Arrays.stream(nums)
+                    .reduce(0, 
+                        (total, i) -> total + i);
+
+            case "*":
+                return Arrays.stream(nums)
+                    .reduce(1, 
+                        (total, i) -> total * i);
+
+            case "-":
+                return Arrays.stream(nums)
+                    .reduce((total, i) -> total - i).getAsInt();
+
+            default:
+                return null;
+        }
+    }
+}
