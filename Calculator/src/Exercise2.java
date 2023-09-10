3fdfa4c49c1a39a10addc8c359eafd776528795f
Eyrim <marfxedbot@gmail.com>
2023-09-10 12:43:24 +0100

    Completed exercise 5.1

--- Calculator/src/Exercise2.java
+++ Calculator/src/Exercise2.java
@@ -1,23 +1,33 @@
 import java.util.Random;
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Exercise2 {
     public static void exercise2() {
-        int[] nums = new int[2];
         Scanner scanner = new Scanner(System.in);
+        int[] nums = getNumsFromUser(scanner);
         String input;
-
-        System.out.println("Enter the first number:");
-        nums[0] = Integer.parseInt(scanner.next());
-        System.out.println("Enter the second number:");
-        nums[1] = Integer.parseInt(scanner.next());
 
         System.out.println("Enter an operation to perform (+, *, -):");
         input = scanner.next();
 
         System.out.println(calculate(input, nums));
         scanner.close();
+    }
+
+    //TODO: 10/09/2023: Potentially stop using the same scanner across multiple methods? Unsure if bad practice
+    private static int[] getNumsFromUser(Scanner scanner) {
+        int[] nums;
+
+        System.out.println("How many numbers?");
+        // Create new array for however many numbers user specified
+        nums = new int[Integer.parseInt(scanner.next())];
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println("Enter number: " + (i+1));
+            nums[i] = Integer.parseInt(scanner.next());
+        }
+
+        return nums;
     }
 
     private static int[] getRandomNums(int quantity) {
